9c79fb6082d44f21e33e4f93dbb09a0359cb9535
Avni Avdulla <avniavdulla@MacBook-3.local>
2020-02-13 15:22:33 -0500

    Avni Added a endpoint

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -3,3 +3,9 @@ module.exports = function(app, db ) {
     res.send('test ping received on test endpoint')
   })
 }
+
+module.exports = function(app, db) {
+  app.post('/test', (req, res) => {
+    res.send('Avnis test ping')
+  })
+}
