f0f936823d2c94405acbe6bc9fbb55cc756ff5a3
Tyler Weisner <weisnert@cse.msu.edu>
2020-04-15 12:39:13 -0400

    working chart.js on homepage.. fake data

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ Our crypto tracker is a GUI for visualizing the history of cryptocurrencies over
 
 ## Why is it important?
 It is a great interactive piece of software that will be outward facing allowing people who visit our site to see something they're familiar with.  It will be a great connector for tying together cryptocurrencies and blockchain on the SBS site.
+
+
+### How to run
+1. terminal command to start up the server and connect to database: 
+`npm run dev`
+2. Open `localhost:8000` in any browser to view the chart
+
+
+
+
+
+
+#### Supplemental Sources
+* https://www.youtube.com/watch?v=sE08f4iuOhA
+* https://vegibit.com/render-html-in-node-js/
+* https://www.chartjs.org/docs/latest/axes/labelling.html
--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express       = require("express");
 const mongoose      = require("mongoose");
 const app           = express();
 const bodyParser    = require('body-parser');
-const coinGeckoAPI  = require('./coinGecko.api')
-// const chart = require()
-
+const coinGeckoAPI  = require('./coinGecko.api');
+// const Chart         = require('./bitcoin');
 require('dotenv/config');
 
 //Import routes 
@@ -12,12 +11,17 @@ const coinRoute = require('./routes/coins');
 app.use(bodyParser.json());
 app.use('/coins', coinRoute);
 
-
+ 
 //Conect to MongoDB
 mongoose.connect(process.env.DB_CONNECTION,
 { useUnifiedTopology: true, 
-    useNewUrlParser: true }, () =>{ console.log('connected to db')
+    useNewUrlParser: true }, () =>{ console.log('connected to db');
 })
+
+// Homepage for express app
+app.get('/', (req, res) => {
+  handleRequest(req, res);
+});
 
 app.post('/bitcoin-history', (req, res) => {
     // coinGeckoAPI.getBitcoinHist(req, res);
@@ -30,3 +34,21 @@ app.listen(port, () => {
     console.log("Server is running on port " + port);
   });
 
+
+let http  = require('http');
+let fs    = require('fs');
+  
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./html/index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
